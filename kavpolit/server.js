45256902b06e7f72806dd9c8c4b57ff16114a7cd
Ramil Aliev <bezdonas@gmail.com>
2018-11-13 23:49:56 +0300

    Kavpolit

---
+++ kavpolit/server.js
@@ -0,0 +1,11 @@
+/*
+ *	Server
+ */
+var express = require('express'),
+    app = express();
+
+app.configure(function () {
+    app.use(express.static(__dirname + '/'));
+});
+
+var server = app.listen(8181);
